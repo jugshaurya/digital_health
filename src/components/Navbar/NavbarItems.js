9ff814de8eb47c9fe444c9c09df6a514fd52a047
Himanshi Gupta <himanshigupta579@gmail.com>
2021-11-21 23:28:24 +0530

    Homepage (#8)
    * homepage added
    
    * homepage updated layout
    
    * homepage layout updated

---
+++ src/components/Navbar/NavbarItems.js
@@ -0,0 +1,21 @@
+//why we declare variable like this
+//why "" not accepted
+const NavbarItems=[
+    {
+        itemName:'Home',
+        url: '#',
+        cName:'Menu-Links'
+    },
+    {
+        itemName:'Login',
+        url: '/Login',
+        cName:'Menu-Links'
+    },
+    {
+        itemName:'SignUp',
+        url: '/signup',
+        cName:'Menu-Links'
+    }
+]
+
+export default NavbarItems
