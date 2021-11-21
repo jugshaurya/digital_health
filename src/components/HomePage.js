9402cb9f31a1f37ff1404de2eb56253f456b2484
himanshi-gupta <himanshigupta579@gmail.com>
2021-11-21 13:56:19 +0530

    homepage updated layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import doctors from '../images/doctors.svg'
+import homepage from '../images/homepage.svg'
+import medication from '../images/medication.svg'
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -14,21 +17,24 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const useStyles = makeStyles({
-  start:{
-    marginTop:'100vh',
-  },
   container:{
     marginRight:'30px',
-    height: '17vh',
+    height: '35vh',
+  },
+  mainImage:{
+    height :'50vh',
+  },
+  gridImage:{
+    height :'17vh',
   },
   grid: {
     // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     // border: 0,
     // borderRadius: 3,
     // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    backgroundColor:'#F0F0F0',
+    // backgroundColor:'#F0F0F0',
     color: 'white',
-    height: '17vh',
+    height: '35vh',
     padding: '0 30px',
     marginLeft:'30px',
   },
@@ -50,77 +56,92 @@ export default function HomePage() {
       <br/>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item xs={6}>
-          <Item>
+          <Item className={classes.mainImage}>
             <h1>Health ID Card</h1>
             <p>Digital book for medical history, analysis, and suggestions</p>
           </Item>
         </Grid>
         <Grid item xs={6}>
-          <Item>2</Item>
+          <Item>
+            <img src={homepage} className={classes.mainImage}/>
+          </Item>
         </Grid>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
       </Grid>
-      <Grid container spacing={2} className={classes.start}>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Grid container spacing={2}>
         <Grid item xs={4}>
           <Item className={classes.grid}>
+            <img src={homepage} className={classes.gridImage}/>
             <h3>Sign Up</h3>
             <p>Create your health ID by using your mobile no. or Aadhaar.</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item className={classes.grid}>
+            <img src={homepage} className={classes.gridImage}/>
             <h3>Personal Health Record</h3>
             <p>Login using your Health ID and view your medical history</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item className={classes.grid, classes.container}>
+          <img src={homepage} className={classes.gridImage}/>
             <h3>Child Health ID</h3>
             <p>Create health ID for your child and keep health record right from birth.</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item className={classes.grid}>
+          <img src={homepage} className={classes.gridImage}/>
             <h3>access to doctors</h3>
             <p>Only doctors exmamining you can access your records.</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item className={classes.grid}>
+          <img src={homepage} className={classes.gridImage}/>
             <h3>Tips/Suggestions</h3>
             <p>Get suggestions on healthy / nutritional food habits and routine exercise or yoga.</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item className={classes.grid, classes.container}>
+          <img src={homepage} className={classes.gridImage}/>
             <h3>Routine chekups</h3>
             <p>Provide you details for next appointment</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item className={classes.grid}>
+          <img src={homepage} className={classes.gridImage}/>
             <h3>Vaccination record</h3>
             <p>View vaccination record and upcoming vaccinations list.</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
-          <Item>
-            <h3></h3>
-            <p></p>
+          <Item className={classes.grid}>
+          <img src={homepage} className={classes.gridImage}/>
+            <h3>Vaccination record</h3>
+            <p>View vaccination record and upcoming vaccinations list.</p>
           </Item>
         </Grid>
         <Grid item xs={4}>
-          <Item>
-            <h3></h3>
-            <p></p>
+          <Item className={classes.grid}>
+          <img src={homepage} className={classes.gridImage}/>
+            <h3>Vaccination record</h3>
+            <p>View vaccination record and upcoming vaccinations list.</p>
           </Item>
         </Grid>
       </Grid>
--- src/index.js
+++ src/index.js
@@ -4,34 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-<<<<<<< HEAD
-/* Routing */
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import DoctorPage from './components/DoctorPage';
-import PatientPage from './components/PatientPage';
-import AdminPage from './components/AdminPage';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login/doctor" element={<DoctorPage />} />
-        <Route path="/login/patient" element={<PatientPage />} />
-        <Route path="/login/admin" element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
-=======
 ReactDOM.render(
   <React.StrictMode>
     <App />
->>>>>>> main
   </React.StrictMode>,
   document.getElementById('root')
 );
